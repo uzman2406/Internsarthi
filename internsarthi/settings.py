dd35f0628beaa6e1aaf8a2bcfaa741910b7ea069
Uzma Nayab <anushauzma@gmail.com>
2025-12-20 19:05:06 +0530

    Update settings.py

--- internsarthi/settings.py
+++ internsarthi/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-ytejvc9%0nh#qs5^mki-(evob501eza2y=u%up#3*yj7m!#nn)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "internsarthi.onrender.com",
+    "localhost",
+    "127.0.0.1",
+
+]
 
 
 # Application definition
